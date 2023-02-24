75c87d7de78164e98fcac2f7660998541f94c7a1
yunari26 <uawasiran3@gmail.com>
2023-02-25 00:29:05 +0900

    MySQL基本編第23回まで

--- main.sql
+++ main.sql
@@ -3,23 +3,30 @@ CREATE TABLE posts (
   id INT NOT NULL AUTO_INCREMENT,
   message VARCHAR(140),
   likes INT,
+  created DATETIME,
   PRIMARY KEY (id)
 );
 
-INSERT INTO posts (message, likes) VALUES
-  ('Thanks', 12),
-  ('Arigato', 4),
-  ('Merci', 4),
-  ('Gracias', 15),
-  ('Danke', 23);
+INSERT INTO posts (message, likes, created) VALUES
+  ('Thanks', 12, '2020-05-01'),
+  ('Merci', 4, '2020-05-03'),
+  ('Arigato', 4, '2020-06-14'),
+  ('Gracias', 15, '2020-07-04'),
+  ('Danke', 8, '2020-08-22');
+  
+-- SELECT created, YEAR(created) FROM posts;
+-- SELECT created, MONTH(created) FROM posts;
+-- SELECT created, DAY(created) FROM posts;
 
--- SELECT * FROM posts;
--- SELECT id, message FROM posts;
-
--- > >= < <= 
--- SELECT * FROM posts WHERE likes >= 10;
-
--- = != <>
-SELECT * FROM posts WHERE message = 'Danke';
-SELECT * FROM posts WHERE message != 'Danke';
-SELECT * FROM posts WHERE message <> 'Danke';
+SELECT
+  created,
+  DATE_FORMAT(created, '%M %D %Y, %W') AS date
+FROM
+  posts;
+  
+SELECT
+  created,
+  NOW(),
+  DATEDIFF(created, NOW()) AS diff
+FROM
+  posts;
