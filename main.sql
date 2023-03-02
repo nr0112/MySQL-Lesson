a5c52ff12e6712c1c3c8d7fa8196d96fe0068b99
yunari26 <uawasiran3@gmail.com>
2023-03-02 16:38:53 +0900

    MySQL基本編最後まで

--- main.sql
+++ main.sql
@@ -3,30 +3,22 @@ CREATE TABLE posts (
   id INT NOT NULL AUTO_INCREMENT,
   message VARCHAR(140),
   likes INT,
-  created DATETIME,
   PRIMARY KEY (id)
 );
 
-INSERT INTO posts (message, likes, created) VALUES
-  ('Thanks', 12, '2020-05-01'),
-  ('Merci', 4, '2020-05-03'),
-  ('Arigato', 4, '2020-06-14'),
-  ('Gracias', 15, '2020-07-04'),
-  ('Danke', 8, '2020-08-22');
+INSERT INTO posts (message, likes) VALUES
+  ('Thanks', 12),
+  ('Merci', 4),
+  ('Arigato', 4),
+  ('Gracias', 15),
+  ('Danke', 8);
   
--- SELECT created, YEAR(created) FROM posts;
--- SELECT created, MONTH(created) FROM posts;
--- SELECT created, DAY(created) FROM posts;
-
-SELECT
-  created,
-  DATE_FORMAT(created, '%M %D %Y, %W') AS date
-FROM
-  posts;
-  
-SELECT
-  created,
-  NOW(),
-  DATEDIFF(created, NOW()) AS diff
-FROM
-  posts;
+-- ALTER TABLE posts ADD author VARCHAR(255);
+-- ALTER TABLE posts ADD author VARCHAR(255) AFTER id;
+-- ALTER TABLE posts ADD author VARCHAR(255) FIRST;
+-- ALTER TABLE posts DROP message;
+-- ALTER TABLE posts CHANGE likes points INT;
+DROP TABLE IF EXISTS messages;
+ALTER TABLE posts RENAME messages;
+SHOW TABLES;
+-- DESC posts;
