94b3ef7bc1e7763522209a20afa7b17dc8c026a4
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2023-03-29 22:16:57 +0900

    Create 5.sql

---
+++ MySQL入門 基礎編/5.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  message VARCHAR(140),
+  likes INT
+);
+
+INSERT INTO posts (message, likes) VALUES
+  ('Thanks', 12),
+  ('Arigato', 4);
+
+SELECT * FROM posts;
