b6b66e92a4a965ca81343ebf40e6fce825672a0c
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2023-03-29 22:41:46 +0900

    Create 17.sql

---
+++ MySQL入門 基礎編/17.sql
@@ -0,0 +1,16 @@
+　DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(140),
+  likes INT,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts (message, likes) VALUES
+  ('Thank you!', 12),
+  ('thanks 100%', 4),
+  ('Gracias', 4),
+  ('Arigato_gozaimasu', 15),
+  ('Arigato! desu', 23);
+
+SELECT * FROM posts WHERE message LIKE 't%!'
