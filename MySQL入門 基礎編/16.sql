cc6f8cc2d6c6ed075a7a3eb457ad426ae576fdc7
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2023-03-29 22:38:39 +0900

    Create 16.sql

---
+++ MySQL入門 基礎編/16.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(140),
+  likes INT,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts (message, likes) VALUES
+  ('Thanks', 12),
+  ('Arigato', 4),
+  ('Merci', 4),
+  ('Gracias', 15),
+  ('Danke', 23);
+
+SELECT * FROM posts WHERE likes BETWEEN 10 AND 20;
+SELECT * FROM posts WHERE likes IN(4, 20);
