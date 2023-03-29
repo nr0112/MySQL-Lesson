c53545cb08d60f2c8c8c3245eadea86c6c14181f
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2023-03-29 23:27:57 +0900

    Create 23.sql

---
+++ MySQL入門 基礎編/23.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(140),
+  likes INT,
+  created DATETIME,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts (message, likes, created) VALUES
+  ('Thanks', 12, '2020-05-01'),
+  ('Merci', 4, '2020-05-03'),
+  ('Arigato', 4, '2020-06-14'),
+  ('Gracias', 15, '2020-07-04'),
+  ('Danke', 8, '2020-08-22');
+  
+SELECT created, YEAR(created) FROM posts;
+SELECT created, MONTH(created) FROM posts;
+SELECT created, DAY(created) FROM posts;
+
+SELECT
+  created,
+  DATE_FORMAT(created, '%M %D %Y, %W') as date
+FROM posts;
+
+SELECT
+  created,
+  DATE_ADD(created, INTERVAL 7 DAY) as after
+FROM posts;
+
+SELECT
+  created,
+  NOW(),
+  DATEDIFF(created, NOW()) as after
+FROM posts;
