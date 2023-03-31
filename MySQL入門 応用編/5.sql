e0b15265ad3936fd2fa70bc6611231b58b4279b9
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2023-03-31 14:13:17 +0900

    Create 5.sql

---
+++ MySQL入門 応用編/5.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(140),
+  likes INT,
+  area VARCHAR(20),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts (message, likes, area) VALUES
+  ('post-1', 12, 'Tokyo'),
+  ('post-2', 8, 'Fukuoka'),
+  ('post-3', 11, 'Tokyo'),
+  ('post-4', 3, 'Osaka'),
+  ('post-5', 8, 'Tokyo'),
+  ('post-6', 9, 'Osaka'),
+  ('post-7', 4, 'Tokyo'),
+  ('post-8', 10, 'Osaka'),
+  ('post-9', 31, 'Fukuoka');
+
+DROP TABLE IF EXISTS posts_tokyo;
+CREATE TABLE posts_tokyo AS SELECT * FROM posts WHERE area = 'Tokyo';
+DROP TABLE IF EXISTS posts_copy;
+CREATE TABLE posts_copy AS SELECT * FROM posts;
+DROP TABLE IF EXISTS posts_skelton;
+CREATE TABLE posts_skelton LIKE posts;
+
+SHOW TABLES;
+SELECT * FROM posts_tokyo;
+SELECT * FROM posts_copy;
+SELECT * FROM posts_skelton;
