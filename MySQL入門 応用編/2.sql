8c3d23d81b26340292b7283d4e88832293237fd9
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2023-03-31 13:40:10 +0900

    Create 2.sql

---
+++ MySQL入門 応用編/2.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(140),
+  likes INT,
+  area VARCHAR(20),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts (message, likes, area) VALUES
+  ('post-1', 12, 'Tokyo'),
+  ('post-2', 8, 'Fukuoka'),
+  ('post-3', 11, 'Tokyo'),
+  ('post-4', 3, 'Osaka'),
+  ('post-5', 8, 'Tokyo'),
+  ('post-6', 9, 'Osaka'),
+  ('post-7', 4, 'Tokyo'),
+  ('post-8', 10, 'Osaka'),
+  ('post-9', 31, 'Fukuoka');
+
+SELECT * FROM posts;
+SELECT  DISTINCT area FROM posts;
+SELECT  area, SUM(likes) FROM posts GROUP BY area;
