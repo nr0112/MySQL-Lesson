abc28deede068245eccb9e4f135f41dc80b6b183
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2023-04-02 23:27:37 +0900

    Create 9.sql

---
+++ MySQL入門 応用編/9.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (
+  id INT NOT NULL AUTO_INCREMENT,
+  message VARCHAR(140),
+  likes INT,
+  area VARCHAR(20),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts (message, likes, area) VALUES
+  ('post-1', 12, 'Tokyo'),
+  ('post-2', 8, 'Fukuoka'),
+  ('post-3', 11, 'Tokyo'),
+  ('post-4', 3, 'Osaka'),
+  ('post-5', 8, 'Tokyo'),
+  ('post-6', 9, 'Osaka'),
+  ('post-7', 4, 'Tokyo'),
+  ('post-8', 10, 'Osaka'),
+  ('post-9', 31, 'Fukuoka');
+
+SELECT
+  *,
+  (SELECT AVG(likes) FROM posts) AS avg,
+  (SELECT AVG(likes) FROM posts AS t2 WHERE t1.area = t2.area) AS area_avg
+FROM
+  posts AS t1;
